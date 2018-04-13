8ecb50b460e2b87dd3a1802981fc0564a9ebec5c
deepakrameshrao <deepak.rao@practo.com>
2018-04-13 08:43:45 +0530

    First commit

---
+++ app/src/main/java/com/raos/signaturelib/Signature.java
@@ -0,0 +1,160 @@
+package com.raos.signaturelib;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
+public class Signature extends View {
+
+    private static final float STROKE_WIDTH = 5f;
+    private static final float HALF_STROKE_WIDTH = STROKE_WIDTH / 2;
+    private Paint paint = new Paint();
+    private Path path = new Path();
+
+    private float lastTouchX;
+    private float lastTouchY;
+    private final RectF dirtyRect = new RectF();
+    private View mContentView;
+    private Bitmap bitmap;
+    private SignatureListener mSignatureListener;
+
+    public Signature(Context context) {
+        this(context, null, null, null);
+    }
+
+    public Signature(Context context, AttributeSet attrs) {
+        this(context, attrs, null, null);
+    }
+
+
+    public Signature(Context context, AttributeSet attrs, View contentView, SignatureListener signatureListener) {
+        super(context, attrs);
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeWidth(STROKE_WIDTH);
+        mContentView = contentView;
+        mSignatureListener = signatureListener;
+    }
+
+    public void save(View v, String StoredPath) {
+        Log.v("log_tag", "Width: " + v.getWidth());
+        Log.v("log_tag", "Height: " + v.getHeight());
+        if (bitmap == null) {
+            bitmap = Bitmap.createBitmap(mContentView.getWidth(), mContentView.getHeight(), Bitmap.Config.RGB_565);
+        }
+        Canvas canvas = new Canvas(bitmap);
+        try {
+            // Output the file
+            FileOutputStream fileOutputStream = new FileOutputStream(StoredPath);
+            v.draw(canvas);
+
+            // Convert the output file to Image such as .png
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, e.toString());
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
+
+    }
+
+    public void clear() {
+        path.reset();
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float eventX = event.getX();
+        float eventY = event.getY();
+        //TODO: Create interface from activity to get save button enabled
+        //mSignatureListener.setSignatureIsDrawn(true);
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(eventX, eventY);
+                lastTouchX = eventX;
+                lastTouchY = eventY;
+                return true;
+
+            case MotionEvent.ACTION_MOVE:
+
+            case MotionEvent.ACTION_UP:
+
+                resetDirtyRect(eventX, eventY);
+                int historySize = event.getHistorySize();
+                for (int i = 0; i < historySize; i++) {
+                    float historicalX = event.getHistoricalX(i);
+                    float historicalY = event.getHistoricalY(i);
+                    expandDirtyRect(historicalX, historicalY);
+                    path.lineTo(historicalX, historicalY);
+                }
+                path.lineTo(eventX, eventY);
+                break;
+
+            default:
+                //debug("Ignored touch event: " + event.toString());
+                return false;
+        }
+
+        invalidate((int) (dirtyRect.left - HALF_STROKE_WIDTH),
+                (int) (dirtyRect.top - HALF_STROKE_WIDTH),
+                (int) (dirtyRect.right + HALF_STROKE_WIDTH),
+                (int) (dirtyRect.bottom + HALF_STROKE_WIDTH));
+
+        lastTouchX = eventX;
+        lastTouchY = eventY;
+        invalidate();
+
+        return true;
+    }
+
+    private void expandDirtyRect(float historicalX, float historicalY) {
+        if (historicalX < dirtyRect.left) {
+            dirtyRect.left = historicalX;
+        } else if (historicalX > dirtyRect.right) {
+            dirtyRect.right = historicalX;
+        }
+
+        if (historicalY < dirtyRect.top) {
+            dirtyRect.top = historicalY;
+        } else if (historicalY > dirtyRect.bottom) {
+            dirtyRect.bottom = historicalY;
+        }
+    }
+
+    private void resetDirtyRect(float eventX, float eventY) {
+        dirtyRect.left = Math.min(lastTouchX, eventX);
+        dirtyRect.right = Math.max(lastTouchX, eventX);
+        dirtyRect.top = Math.min(lastTouchY, eventY);
+        dirtyRect.bottom = Math.max(lastTouchY, eventY);
+    }
+
+    public interface SignatureListener {
+        void setSignatureIsDrawn(boolean signatureDrawn);
+    }
+}
